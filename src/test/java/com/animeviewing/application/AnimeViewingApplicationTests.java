e9d5c9ffa9080d626dbe925a64349a913fae2d07
taktin62 <77627702+taktin62@users.noreply.github.com>
2022-06-14 22:40:17 +0900

    アニメタイトル一覧画面新規登録　ControllerとHTML作成 (#1)
    * アニメタイトル一覧画面新規登録　ControllerとHTML作成

---
+++ src/test/java/com/animeviewing/application/AnimeViewingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.animeviewing.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimeViewingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
