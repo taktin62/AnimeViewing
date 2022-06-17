394e18cd266fbfd8adbab8e5b541095db859d734
taktin62 <takuya199662@gmail.com>
2022-06-17 21:40:44 +0900

    AnimationRepositoryはInterfaceクラスとAnimationクラスとAnimationServiceクラス 作成

---
+++ src/main/java/com/animeviewing/application/entities/Animation.java
@@ -0,0 +1,57 @@
+package com.animeviewing.application.entities;
+
+/**
+ * アニメーションエンティティ
+ */
+public class Animation {
+	/**
+	 * フィールド
+	 */
+	private Integer animationId;
+	private String title;
+	private String genre;
+	private String authorId;
+	private String publisherId;
+	private String magazineId;
+
+	/**
+	 * コンストラクター
+	 */
+	public Animation(Integer animationId, String title, String genre, String authorId, String publisherId,
+			String magazineId) {
+		this.animationId = animationId;
+		this.title = title;
+		this.genre = genre;
+		this.authorId = authorId;
+		this.publisherId = publisherId;
+		this.magazineId = magazineId;
+	}
+
+	/**
+	 * getterメソッド
+	 */
+	public Integer getAnimationId() {
+		return animationId;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public String getAuthorId() {
+		return authorId;
+	}
+
+	public String getPublisherId() {
+		return publisherId;
+	}
+
+	public String getMagazineId() {
+		return magazineId;
+	}
+
+}
---
+++ src/main/java/com/animeviewing/application/services/AnimationService.java
@@ -0,0 +1,31 @@
+package com.animeviewing.application.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.animeviewing.application.entities.Animation;
+import com.animeviewing.application.repositories.AnimationRepository;
+
+/**
+ * アニメーションサービスクラス
+ */
+@Service
+@Transactional
+public class AnimationService {
+	@Autowired
+	AnimationRepository animationRepository;
+
+	/**
+	 * アニメ一覧取得
+	 * 20件ずつページネーション
+	 */
+	public List<Animation> getList(Pageable pageable) {
+
+		return animationRepository.findAll(pageable);
+	}
+
+}
