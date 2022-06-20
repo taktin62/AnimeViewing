f5726b6ec4954358eb9f5b0be6c2100463b241f3
taktin62 <takuya199662@gmail.com>
2022-06-20 17:15:59 +0900

    文字化けしていたのでUTF-8に変更

--- src/main/resources/ddl/select_findAll_title.sql
+++ src/main/resources/ddl/select_findAll_title.sql
@@ -1,15 +1,15 @@
 --2022/06/20
---全件取得_サンプル
+--蜈ｨ莉ｶ蜿門ｾ誉繧ｵ繝ｳ繝励Ν
 SELECT title, genre, author_id, publisher_id, magazine
 	FROM animation_view.animations, animation_view.genre;
 
---全件取得VER01
+--蜈ｨ莉ｶ蜿門ｾ遊ER01
 SELECT title, genre, author_id, publisher_id, name
 	FROM animation_view.animations, animation_view.publish
 		INNER JOIN animation_view.magazines ON animation_view.publish.magazine_id = animation_view.magazines.name;
 
-----全件取得VER02
+----蜈ｨ莉ｶ蜿門ｾ遊ER02
 SELECT title, genre, author_id, publisher_id, magazine_id
 	FROM animation_view.animations, animation_view.publish
-		INNER JOIN animation_view.publish ON animation_view.animations = animation_view.publish.publisher_id  
+		INNER JOIN animation_view.publish ON animation_view.animations.publisher_id = animation_view.publish.publisher_id
 		INNER JOIN animation_view.magazines ON animation_view.publish.magazine_id = animation_view.magazines.magazine_id;
