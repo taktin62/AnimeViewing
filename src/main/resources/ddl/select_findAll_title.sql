3b502407aa936c8c60b030c37582b6df116908c3
taktin62 <takuya199662@gmail.com>
2022-06-21 12:19:09 +0900

    アニメタイトル全件取得 VER03と04作成

--- src/main/resources/ddl/select_findAll_title.sql
+++ src/main/resources/ddl/select_findAll_title.sql
@@ -1,5 +1,5 @@
 --2022/06/20
---全件取得_サンプル
+--全件取得サンプル
 SELECT title, genre, author_id, publisher_id, magazine
 	FROM animation_view.animations, animation_view.genre;
 
@@ -8,8 +8,20 @@ SELECT title, genre, author_id, publisher_id, name
 	FROM animation_view.animations, animation_view.publish
 		INNER JOIN animation_view.magazines ON animation_view.publish.magazine_id = animation_view.magazines.name;
 
-----全件取得VER02
+--全件取得VER02
 SELECT title, genre, author_id, publisher_id, magazine_id
 	FROM animation_view.animations, animation_view.publish
 		INNER JOIN animation_view.publish ON animation_view.animations.publisher_id = animation_view.publish.publisher_id
 		INNER JOIN animation_view.magazines ON animation_view.publish.magazine_id = animation_view.magazines.magazine_id;
+
+--全件取得VER03
+SELECT title, genre, author_id, publisher_id, magazine_id
+	FROM Aanimation_view.animations, animation_view.publish
+		INNER JOIN animation_view.authors ON animation_view.animations.author_id = animation_view.authors.author_id
+		INNER JOIN animation_view.publishers ON animation_view.animations.publisher_id = animation_view.publishers.publisher_id
+		INNER JOIN animation_view.magazines ON animation_view.publish.magazine_id = animation_view.magazines.magazine_id;
+
+--全件取得VER04　2022/6/21
+SELECT a.title, a.genre, a.author_id, a.publisher_id, p.magazine_id
+	FROM animation_view.animations a, animation_view.publish p
+		INNER JOIN animation_view.magazines m ON p.magazine_id = m.magazine_id;
