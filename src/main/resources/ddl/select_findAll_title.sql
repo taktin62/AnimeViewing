fcc35f03b790ba24fc90911081df41475f02273c
taktin62 <takuya199662@gmail.com>
2022-06-22 12:00:35 +0900

    アニメタイトル 全件取得 SQL文VER06-1,2作成 （最新）

--- src/main/resources/ddl/select_findAll_title.sql
+++ src/main/resources/ddl/select_findAll_title.sql
@@ -21,7 +21,29 @@ SELECT title, genre, author_id, publisher_id, magazine_id
 		INNER JOIN animation_view.publishers ON animation_view.animations.publisher_id = animation_view.publishers.publisher_id
 		INNER JOIN animation_view.magazines ON animation_view.publish.magazine_id = animation_view.magazines.magazine_id;
 
---全件取得VER04　2022/6/21
-SELECT a.title, a.genre, a.author_id, a.publisher_id, p.magazine_id
+--全件取得VER04 2022/6/21
+SELECT a.title, a.genre, a.author_id, a.publisher_id, m.magazine_id
 	FROM animation_view.animations a, animation_view.publish p
 		INNER JOIN animation_view.magazines m ON p.magazine_id = m.magazine_id;
+
+--全件取得VER05
+SELECT a.title, a.genre, a.author_id, a.publisher_id, m.name
+	FROM animation_view.animations a, animation_view.publish p
+		INNER JOIN animation_view.magazines m ON p.magazine_id = m.magazine_id;
+
+--全件取得VER06-1 2022/6/22 アニメ化されていて漫画化されている
+SELECT a.title, a.genre, t.name, ps.name, m.name
+	FROM animation_view.animations a
+		INNER JOIN animation_view.publish p ON a.animation_id = p.animation_id
+		INNER JOIN animation_view.authors t ON a.author_id = t.author_id
+		INNER JOIN animation_view.publishers ps ON a.publisher_id = ps.publisher_id
+		INNER JOIN animation_view.magazines m ON p.magazine_id = m.magazine_id;
+
+--全件取得VER06-2 2022/6/22 アニメ化されていて漫画化されていないものまたは、アニメ化されていて漫画化されている
+SELECT a.title, a.genre, t.name, ps.name, m.name
+	FROM animation_view.animations a
+		LEFT JOIN animation_view.publish p ON a.animation_id = p.animation_id
+		LEFT JOIN animation_view.magazines m ON p.magazine_id = m.magazine_id
+		INNER JOIN animation_view.authors t ON a.author_id = t.author_id
+		INNER JOIN animation_view.publishers ps ON a.publisher_id = ps.publisher_id;
+
